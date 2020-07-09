a6260d38ec20fa8ceca47784e74e49125fec8e76
Evandro Carlos <evandrorocha20@gmail.com>
2020-07-08 22:07:16 -0300

    setup do migration

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const routes = require('./src/routes')
 const app = express();
 const port = process.env.PORT || 4444;
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 
 app.use(routes);
 
-app.listen(port, err => {
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({ ...error, message: error.message })
+
+});
+
+app.listen(port, (err) => {
     if (err) return console.log(`Não iniciou ${err}`)
     console.log('Running on port', port)
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,8 @@
 const { Router } = require('express')
 const orders = require('./orders')
 const products = require('./products')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
 
 const router = new Router()
-
-router.use(morgan('dev'))
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
-
-router.use((req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*');
-    res.header(
-        'Acess-Control-Allow-Header',
-        'Origin',
-        'X-Requested-With',
-        'Content-Type',
-        'Accept',
-        'Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.header('Acess-Control-Aloow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-})
 
 router.use(orders)
 router.use(products)
@@ -35,15 +11,6 @@ router.use((req, res, next) => {
     const erro = new Error('Não encontrado');
     erro.status = 404;
     next(erro);
-});
-
-router.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
 });
 
 module.exports = router
