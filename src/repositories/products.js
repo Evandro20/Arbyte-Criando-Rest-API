3537cc69b98f55ca877dd602bdd4d640e1f852c4
Evandro Carlos <evandrorocha20@gmail.com>
2020-07-13 22:12:33 -0300

    Refatorando o crud e ainda falta o Patch e alguns service

---
+++ src/repositories/products.js
@@ -0,0 +1,29 @@
+const knex = require('../../database');
+
+const tableName = 'products'
+
+const getAll = () => knex(tableName)
+
+const getById = (id) => {
+    return knex(tableName)
+        .where({ id: id })
+        .then(([product]) => product)
+}
+
+const create = (product) => {
+    return knex(tableName)
+        .insert(product)
+        .then(([inserted]) => inserted);
+};
+
+const del = (id) => {
+    return knex(tableName)
+        .where({ id: id }).del()
+}
+
+module.exports = {
+    getAll,
+    create,
+    getById,
+    del,
+};
